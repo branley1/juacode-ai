df5dac2a6af3292a441cf1c49305cbd3f82fd90b
branley1 <mmasi.branley@gmail.com>
2025-05-29 08:39:29 -0400

    fix: import paths

--- juacode-nextjs-app/src/components/Sidebar/Sidebar.js
+++ juacode-nextjs-app/src/components/Sidebar/Sidebar.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import './Sidebar.css';
 import JuaCodeLogo from '../../assets/jua-code-logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { icons } from '../../utils/icons.ts';
+import { icons } from '../../utils/icons';
 import { faEllipsisV, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 // Safely retrieve the chat id from the chat object.
